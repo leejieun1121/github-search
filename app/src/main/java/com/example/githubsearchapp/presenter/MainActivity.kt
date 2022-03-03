bd0c5605317272805d594214c4a0640bdbdce7e1
캘리_이지은 <lje4648@naver.com>
2022-03-04 01:26:09 +0900

    Activity에서 Fragment로 변경

--- app/src/main/java/com/example/githubsearchapp/presenter/MainActivity.kt
+++ app/src/main/java/com/example/githubsearchapp/presenter/MainActivity.kt
@@ -1,114 +1,16 @@
 package com.example.githubsearchapp.presenter
 
-import android.app.Activity
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.core.widget.doAfterTextChanged
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.paging.LoadState
-import androidx.recyclerview.widget.RecyclerView
 import com.example.githubsearchapp.R
-import com.example.githubsearchapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-@ExperimentalCoroutinesApi
-@FlowPreview
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var imm: InputMethodManager
-    private val viewModel: MainViewModel by viewModels()
-
-    private lateinit var mainPagingAdapter: MainPagingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this@MainActivity
-
-        imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        mainPagingAdapter = MainPagingAdapter()
-        setupViews()
-    }
-
-    private fun setupViews() {
-        binding.rvMain.adapter = mainPagingAdapter.withLoadStateFooter(
-            footer = LoadStateAdapter { mainPagingAdapter.retry() }
-        )
-
-        binding.rvMain.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
-            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-                imm.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
-                binding.etSearch.clearFocus()
-                return false
-            }
-
-            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
-            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
-        })
-
-        mainPagingAdapter.addLoadStateListener { loadState ->
-            binding.tvNothing.isVisible =
-                loadState.refresh is LoadState.NotLoading && mainPagingAdapter.itemCount == 0 && binding.etSearch.text.isNotEmpty()
-        }
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                mainPagingAdapter.loadStateFlow
-                    .distinctUntilChangedBy { it.refresh }
-                    .filter { it.refresh is LoadState.NotLoading }
-                    .collect { binding.rvMain.scrollToPosition(0) }
-            }
-        }
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                textChanges()
-                    .debounce(500)
-                    .collectLatest { keyword ->
-                        viewModel.getRepoList(keyword)
-                    }
-            }
-        }
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.repoList.collectLatest { repoInfo ->
-                    mainPagingAdapter.submitData(repoInfo)
-                }
-            }
-        }
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.error.collectLatest { message ->
-                    Toast.makeText(this@MainActivity,message,Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-
-    }
-
-    private fun textChanges() = callbackFlow {
-        val listener = binding.etSearch.doAfterTextChanged { keyword ->
-            trySend(keyword.toString())
-        }
-        awaitClose { binding.etSearch.removeTextChangedListener(listener) }
+        setContentView(R.layout.activity_main)
     }
 
 }
---
+++ app/src/main/java/com/example/githubsearchapp/presenter/SearchFragment.kt
@@ -0,0 +1,119 @@
+package com.example.githubsearchapp.presenter
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.RecyclerView
+import com.example.githubsearchapp.R
+import com.example.githubsearchapp.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+@ExperimentalCoroutinesApi
+@FlowPreview
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var imm: InputMethodManager
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var mainPagingAdapter: MainPagingAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        imm =
+            requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        mainPagingAdapter = MainPagingAdapter()
+        setupViews()
+        return binding.root
+    }
+
+    private fun setupViews() {
+        binding.rvMain.adapter = mainPagingAdapter.withLoadStateFooter(
+            footer = LoadStateAdapter { mainPagingAdapter.retry() }
+        )
+
+        binding.rvMain.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                imm.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
+                binding.etSearch.clearFocus()
+                return false
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+        })
+
+        mainPagingAdapter.addLoadStateListener { loadState ->
+            binding.tvNothing.isVisible =
+                loadState.refresh is LoadState.NotLoading && mainPagingAdapter.itemCount == 0 && binding.etSearch.text.isNotEmpty()
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainPagingAdapter.loadStateFlow
+                    .distinctUntilChangedBy { it.refresh }
+                    .filter { it.refresh is LoadState.NotLoading }
+                    .collect { binding.rvMain.scrollToPosition(0) }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                textChanges()
+                    .debounce(500)
+                    .collectLatest { keyword ->
+                        viewModel.getRepoList(keyword)
+                    }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.repoList.collectLatest { repoInfo ->
+                    mainPagingAdapter.submitData(repoInfo)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.error.collectLatest { message ->
+                    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    private fun textChanges() = callbackFlow {
+        val listener = binding.etSearch.doAfterTextChanged { keyword ->
+            trySend(keyword.toString())
+        }
+        awaitClose { binding.etSearch.removeTextChangedListener(listener) }
+    }
+}
